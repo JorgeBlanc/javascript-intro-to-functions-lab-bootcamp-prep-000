91620505f7ff5d1cfcc7eb8d51e6f452cdb48974
George White <thegeorgewilliam@gmail.com>
2018-04-11 22:46:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+//overthought this one by alot,
+//simply convert thhe  funvtion to  call logShout(string)
+// and  put the uppercase input as a consloe command and use argumrnt string to call it
+}
+logShout('string')
+
+
+function logWhisper(string) {
+  console.log('hello')
+}
+logWhisper('string')
+
